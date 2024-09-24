8f2d002e264ad7582b82a9bc727847d12eeb5935
SandKat214 <sandeenk@oregonstate.edu>
2024-09-23 20:33:43 -0700

    sets up contact form validation

--- portfolio-site/src/pages/Contact.jsx
+++ portfolio-site/src/pages/Contact.jsx
@@ -3,6 +3,7 @@ import {
 	Container,
 	Flex,
 	FormControl,
+	FormHelperText,
 	FormLabel,
 	Heading,
 	HStack,
@@ -12,12 +13,49 @@ import {
 	Textarea,
 	VStack,
 } from "@chakra-ui/react"
-import { Form } from "react-router-dom"
+import { Form, useSubmit } from "react-router-dom"
+import { useFormik } from "formik"
+import * as yup from "yup"
 
 // Icons
 import { RiSendPlaneFill } from "react-icons/ri"
 
 const Contact = () => {
+	const submit = useSubmit()
+
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			email: "",
+			subject: "",
+			message: "",
+		},
+		validationSchema: yup.object().shape({
+			name: yup.string().required("Name is required:"),
+			email: yup
+				.string()
+				.matches(
+					/^((?:[A-Za-z0-9!#$%&'*+\-\/=?^_`{|}~]|(?<=^|\.)"|"(?=$|\.|@)|(?<=".*)[ .](?=.*")|(?<!\.)\.){1,64})(@)((?:[A-Za-z0-9.\-])*(?:[A-Za-z0-9])\.(?:[A-Za-z0-9]){1,})$/gm,
+					"Please enter a valid email:"
+				)
+				.required("Email is required:"),
+			subject: yup
+				.string()
+				.required("Subject line is required:")
+				.min(10, "Subject must be at least 10 characters:"),
+			message: yup
+				.string()
+				.required("Message is required:")
+				.min(20, "Message must be at least 20 characters:"),
+		}),
+		onSubmit: async (values) => {
+			submit(values, {
+				method: "post",
+				action: "/contact",
+			})
+		},
+	})
+
 	return (
 		<Container
 			as='section'
@@ -25,7 +63,7 @@ const Contact = () => {
 			boxShadow='0 0 20px #D253FF'
 			borderRadius='20px'
 			p='25px'
-			m={['auto', '20px']}
+			m={["auto", "20px"]}
 			flex={1}
 		>
 			<Heading
@@ -37,32 +75,64 @@ const Contact = () => {
 			>
 				...or via form
 			</Heading>
-			<Form>
-				<VStack spacing='10px'>
+			<Form onSubmit={formik.handleSubmit}>
+				<VStack spacing='8px'>
 					<Flex
 						w='100%'
 						direction={["column", "row", "column", null, "row"]}
-						gap={["8px", "20px", "8px", "20px"]}
+						gap={["8px", "20px", "8px", null, "20px"]}
 					>
 						<FormControl>
-							<FormLabel variant='contact' m='0'>
-								Name:
-							</FormLabel>
+							<FormHelperText
+								my='2px'
+								mx='8px'
+								fontWeight={300}
+								fontStyle='italic'
+								fontSize='xs'
+								color={
+									formik.touched.name && formik.errors.name
+										? "red.500"
+										: "foreground"
+								}
+							>
+								{formik.touched.name && formik.errors.name
+									? formik.errors.name
+									: "Please enter your full name:"}
+							</FormHelperText>
 							<Input
 								type='text'
 								name='name'
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								isRequired
 								size='xs'
 								w='100%'
 								variant='contact'
 							/>
 						</FormControl>
 						<FormControl>
-							<FormLabel variant='contact' m='0'>
-								Email:
-							</FormLabel>
+							<FormHelperText
+								my='2px'
+								mx='8px'
+								fontWeight={300}
+								fontStyle='italic'
+								fontSize='xs'
+								color={
+									formik.touched.email && formik.errors.email
+										? "red.500"
+										: "foreground"
+								}
+							>
+								{formik.touched.email && formik.errors.email
+									? formik.errors.email
+									: "Please enter your email:"}
+							</FormHelperText>
 							<Input
 								type='email'
 								name='email'
+								onChange={formik.handleChange}
+								onBlur={formik.handleBlur}
+								isRequired
 								size='xs'
 								w='100%'
 								variant='contact'
@@ -70,27 +140,62 @@ const Contact = () => {
 						</FormControl>
 					</Flex>
 					<FormControl>
-						<FormLabel variant='contact' m='0'>
-							Subject:
-						</FormLabel>
+						<FormHelperText
+							my='2px'
+							mx='8px'
+							fontWeight={300}
+							fontStyle='italic'
+							fontSize='xs'
+							color={
+								formik.touched.subject && formik.errors.subject
+									? "red.500"
+									: "foreground"
+							}
+						>
+							{formik.touched.subject && formik.errors.subject
+								? formik.errors.subject
+								: "Please enter a subject line:"}
+						</FormHelperText>
 						<Input
 							type='text'
 							name='subject'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							isRequired
+							minLength={10}
 							size='xs'
 							variant='contact'
 						/>
 					</FormControl>
 					<FormControl>
-						<FormLabel variant='contact' m='0'>
-							Message:
-						</FormLabel>
+						<FormHelperText
+							my='2px'
+							mx='8px'
+							fontWeight={300}
+							fontStyle='italic'
+							fontSize='xs'
+							color={
+								formik.touched.message && formik.errors.message
+									? "red.500"
+									: "foreground"
+							}
+						>
+							{formik.touched.message && formik.errors.message
+								? formik.errors.message
+								: "Please enter your message:"}
+						</FormHelperText>
 						<Textarea
 							name='message'
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							isRequired
+							minLength={20}
 							variant='contact'
 							fontSize='xs'
 						/>
 					</FormControl>
 					<Button
+						type='submit'
 						variant='pinkLight'
 						mt='15px'
 						py='5px'
