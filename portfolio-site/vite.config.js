c0421c590cb8c2df8274006dc04d47aec919915c
SandKat214 <sandeenk@oregonstate.edu>
2024-12-27 15:57:03 -0800

    adds ability to specify port

--- portfolio-site/vite.config.js
+++ portfolio-site/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	esbuild: {
+		loader: "jsx",
+	},
+	server: {
+		// custom port
+		port: parseInt(process.env.VITE_PORT, 10) || 5173,
+	},
 })
