88ef769fe60269e0b5906290a4d669323538058b
SandKat214 <sandeenk@oregonstate.edu>
2025-08-10 18:43:36 -0700

    fixes toast fire on projects loading error

--- frontend/src/layouts/ProjectsLayout.jsx
+++ frontend/src/layouts/ProjectsLayout.jsx
@@ -1,6 +1,14 @@
 import PropTypes from "prop-types"
-import { Center, Container, Spinner, VStack, useToast } from "@chakra-ui/react"
-import { useState } from "react"
+import {
+	Button,
+	Center,
+	Container,
+	Heading,
+	Spinner,
+	VStack,
+	useToast,
+} from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 import { Outlet, useOutletContext } from "react-router-dom"
 import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
@@ -16,28 +24,28 @@ const ProjectsLayout = ({ projects }) => {
 	const [isIndex, setIsIndex] = useState(false)
 
 	// Fetch projects from db
-	const { isLoading } = useQuery({
+	const { isLoading, isError, error, refetch, isFetching } = useQuery({
 		queryKey: ["projects"],
 		queryFn: async () => {
-			try {
-				const res = await axios.get(
-					`${import.meta.env.VITE_API}projects/`
-				)
-				projects.sections = res.data
-				return res.data
-			} catch (error) {
-				console.log(error)
-
-				toast({
-					description:
-						error.response.data.error ||
-						"Could not retieve projects from the server.",
-					status: "error",
-				})
-				return error
-			}
+			const res = await axios.get(`${import.meta.env.VITE_API}projects/`)
+			projects.sections = res.data
+			return res.data
 		},
+		retry: 1,
 	})
+
+	// Fire toast on error
+	useEffect(() => {
+		if (isError && error) {
+			console.error(error)
+			toast({
+				description:
+					error.response?.data?.error ||
+					"Could not retrieve projects from server.",
+				status: "error",
+			})
+		}
+	}, [isError, error, toast])
 
 	return (
 		<Container
@@ -46,36 +54,58 @@ const ProjectsLayout = ({ projects }) => {
 			w={["100%", null, "90%"]}
 			px={["25px", "25px", "60px"]}
 		>
-			<VStack
-				as='article'
-				minHeight='67vh'
-				py='30px'
-				maxW='100%'
-				gap='30px'
-			>
-				{isLoading ? (
-					<Center h='100%' w='100%'>
-						<Spinner color='foreground' size='xl' />
-					</Center>
-				) : (
-					<>
-						<LocalNav
-							heading={heading}
-							isIndex={isIndex}
-							isLrgScreen={isLrgScreen}
-							page={projects}
-						/>
-						<Outlet
-							context={{
-								isLrgScreen,
-								projects: projects.sections,
-								setHeading,
-								setIsIndex,
-							}}
-						/>
-					</>
-				)}
-			</VStack>
+			{isError ? (
+				<Center as='article' minHeight='67vh' py='30px' maxW='100%'>
+					<VStack spacing={5}>
+						<Heading
+							as='h2'
+							variant='pinkHalo'
+							fontWeight='bold'
+							fontSize='xl'
+						>
+							Failed to load projects.
+						</Heading>
+						<Button
+							onClick={() => refetch()}
+							isLoading={isFetching}
+							variant='pinkLight'
+						>
+							Retry
+						</Button>
+					</VStack>
+				</Center>
+			) : (
+				<VStack
+					as='article'
+					minHeight='67vh'
+					py='30px'
+					maxW='100%'
+					gap='30px'
+				>
+					{isLoading ? (
+						<Center h='100%' w='100%'>
+							<Spinner color='foreground' size='xl' />
+						</Center>
+					) : (
+						<>
+							<LocalNav
+								heading={heading}
+								isIndex={isIndex}
+								isLrgScreen={isLrgScreen}
+								page={projects}
+							/>
+							<Outlet
+								context={{
+									isLrgScreen,
+									projects: projects.sections,
+									setHeading,
+									setIsIndex,
+								}}
+							/>
+						</>
+					)}
+				</VStack>
+			)}
 		</Container>
 	)
 }
